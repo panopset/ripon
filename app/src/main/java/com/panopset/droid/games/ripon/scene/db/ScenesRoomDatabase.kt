7f1f8ea2dbb6c341af1c3deafef478ab274c0d9d
Karl Dinwiddie <karldinwiddie@gmail.com>
2019-07-30 03:27:20 -0700

    .

--- app/src/main/java/com/panopset/droid/games/ripon/scene/db/ScenesRoomDatabase.kt
+++ app/src/main/java/com/panopset/droid/games/ripon/scene/db/ScenesRoomDatabase.kt
@@ -1,7 +1,33 @@
 package com.panopset.droid.games.ripon.scene.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [Scene::class], version = 1)
-class ScenesRoomDatabase {
+abstract class ScenesRoomDatabase : RoomDatabase() {
+
+    abstract fun scenesDao(): ScenesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ScenesRoomDatabase? = null
+
+        @Synchronized fun getDatabase(
+            context: Context
+        ): ScenesRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ScenesRoomDatabase::class.java,
+                    "scenes_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
